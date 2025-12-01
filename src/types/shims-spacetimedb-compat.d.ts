d6a9b41dc3878252e5a9d4d28ed6f1bb3dddfdd4
Arukh8925 <arukh@users.noreply.github.com>
2025-12-01 15:38:04 +0700

    chore(types): expand spacetimedb shims to cover 1.9 codegen imports (Impl classes + context interfaces)

--- src/types/shims-spacetimedb-compat.d.ts
+++ src/types/shims-spacetimedb-compat.d.ts
@@ -11,8 +11,13 @@ declare module "spacetimedb" {
 
   export type AlgebraicTypeType = any
   export type Infer<T = any> = any
-  export type RemoteModule = any
+  export type RemoteModule<T = any> = any
   export type DbConnectionConfig<T = any> = any
+  export type Event = any
+  export type EventContextInterface<T = any> = any
+  export type SubscriptionEventContextInterface<T = any> = any
+  export type ReducerEventContextInterface<T = any> = any
+  export type ErrorContextInterface<T = any> = any
 
   // builder types referenced by codegen
   export class DbConnectionBuilder<DbConnection = any, ErrorContext = any, SubscriptionEventContext = any> {
@@ -21,5 +26,14 @@ declare module "spacetimedb" {
     withModuleName(name: string): this
     withToken(token?: string): this
     build(): DbConnection
+  }
+
+  // Implementation classes referenced by codegen (shims only)
+  export class SubscriptionBuilderImpl<T = any> {
+    constructor(...args: any[])
+  }
+  export class DbConnectionImpl<T = any> {
+    constructor(...args: any[])
+    static builder?: (...args: any[]) => any
   }
 }
